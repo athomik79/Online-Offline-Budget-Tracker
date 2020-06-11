c211426d659b3a0c39757c55c599ed126c613f7e
Andrew Thomas <athomik79@gmail.com>
2020-06-10 22:05:13 -0400

    preparing for heroku deployment

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const logger = require("morgan")
 const mongoose = require("mongoose");
+const compression = require("compression")
 
 const PORT = process.env.PORT || 3000;
 
